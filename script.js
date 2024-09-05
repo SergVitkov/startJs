f947c4920805f08e3cde56b342fd84b50c34b014
SergVitkov <sonikstar1993@gmail.com>
2024-09-05 21:03:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+// const num = 50;
+
+// switch (num) {
+//     case 49: 
+//         console.log('Not correct');
+//         break;
+
+//     case 100: 
+//         console.log('Not correct');
+//         break;
+    
+//     case 50:
+//         console.log('Yes, it is true');
+//         break;
+
+//     default:
+//         console.log('Not in this time');
+//         break;
+// }
+
+// const hamburger = true;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('Oh yeahh,great')
+// } else {
+//     console.log('Oh no, not today')
+// }
+
+// console.log((hamburger && fries));
+
+// const hamburger = 3;
+// const fries = 0;
+// const cola = 0;
+
+// if (hamburger || fries || cola) {
+//     console.log('Oh yeahh,great')
+// } else {
+//     console.log('Oh no')
+// }
+
+// console.log((hamburger && fries));
+
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || samReport || mariaReport); 
+
+// ЦЫКЛЫ
+// let num = 50;
+
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+
+//     if (i === 6){
+//         // break;
+//         continue; //позволяет продолжить далее операцию но при этом он не выыедет значение в консоль
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 3; i++){
+//     console.log(i);
+//     for (let j = 0; i < 3; j++){
+//         console.log(j);
+        
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += "*"; 
+//     }
+    
+//     result += '\n';
+// }
+
+// console.log(result);
+
+
+// let result = '';
+// const length = 11;
+
+// for(let i = 1; i < length; i++) {
+
+//     for(let j = 0; j < i; j++) {
+//         result += "&";
+//     }
+
+//     result += "\n";
+// }
+
+// console.log(result);
+
+
+
+
+// for(let i = 2; i <= 10; i+=2) {
+//     console.log(i);
+// }
+
+
+
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
+
+
+ 
